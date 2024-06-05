7c2318310195b4c599855e0be4846fd90508ee2c
Marc <cmdemoral@gmail.com>
2024-06-05 20:57:46 +0800

    Created File System Mover service Added installer Added log4net logging and event viewer logging

---
+++ FileMoverExam.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.ServiceProcess;
+using System.Threading;
+using log4net;
+using log4net.Config;
+using NLog;
+
+namespace FileMoverExam
+{
+    public partial class FileMoverExam : ServiceBase
+    {
+        private FileSystemWatcher watcher;
+        private static readonly ILog log = log4net.LogManager.GetLogger(typeof(FileMoverExam));
+
+        public FileMoverExam()
+        {
+            InitializeComponent();
+            ConfigureLog4Net();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            log.Info("Service started.");
+            SetUpWatcher();
+        }
+
+        protected override void OnStop()
+        {
+            log.Info("Service stopped.");
+            watcher.Dispose();
+        }
+
+        private void SetUpWatcher()
+        {
+            watcher = new FileSystemWatcher
+            {
+                Path = @"C:\Folder1",
+                NotifyFilter = NotifyFilters.FileName | NotifyFilters.CreationTime,
+                Filter = "*.*"
+            };
+
+            watcher.Created += OnCreated;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void OnCreated(object sender, FileSystemEventArgs e)
+        {
+            try
+            {
+                string targetPath = Path.Combine(@"C:\Folder2", Path.GetFileName(e.FullPath));
+                File.Move(e.FullPath, targetPath);
+                log.Info($"File moved: {e.FullPath} to {targetPath}");
+            }
+            catch (Exception ex)
+            {
+                log.Error($"Error moving file: {ex.Message}");
+            }
+        }
+
+        private void ConfigureLog4Net()
+        {
+            XmlConfigurator.Configure(new FileInfo(AppDomain.CurrentDomain.BaseDirectory + "log4net.config"));
+        }
+    }
+}
---
+++ ProjectInstaller.Designer.cs
@@ -0,0 +1,69 @@
+﻿using System.ServiceProcess;
+
+namespace FileMoverService
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serviceProcessInstaller1 = new System.ServiceProcess.ServiceProcessInstaller();
+            this.serviceInstaller1 = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // serviceProcessInstaller1
+            // 
+
+            // Run the Service under Local Service Account
+            serviceProcessInstaller1.Account = ServiceAccount.LocalService;
+
+            this.serviceProcessInstaller1.Password = null;
+            this.serviceProcessInstaller1.Username = null;
+
+
+
+            // 
+            // serviceInstaller1
+            // 
+            this.serviceInstaller1.Description = "File Mover Exam";
+            this.serviceInstaller1.DisplayName = "File Mover Exam";
+            this.serviceInstaller1.ServiceName = "FileMoverExam";
+
+
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.serviceProcessInstaller1,
+            this.serviceInstaller1});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller serviceProcessInstaller1;
+        private System.ServiceProcess.ServiceInstaller serviceInstaller1;
+    }
+}
